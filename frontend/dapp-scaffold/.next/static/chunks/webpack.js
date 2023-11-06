4c74ce4c5dd15e37293994f962306a353e5d6328
knedlicc <kisstrust@icloud.com>
2023-11-06 22:42:25 +0000

    wip:styles added

--- frontend/dapp-scaffold/.next/static/chunks/webpack.js
+++ frontend/dapp-scaffold/.next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdebe6bb969b00e9"; }
+/******/ 		__webpack_require__.h = function() { return "85d6e05923ef408f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/dapp-scaffold/src/views/home/index.tsx
+++ frontend/dapp-scaffold/src/views/home/index.tsx
@@ -1,7 +1,6 @@
 // Next, React
 import { FC, useEffect, useState } from 'react';
 import Link from 'next/link';
-
 // Wallet
 import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
